181cf8ce9fa26e01a7cda0f95fa845d4e4462367
Yuu Feuring <78978984+yuu8812@users.noreply.github.com>
2023-01-18 03:38:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const TerserPlugin = require('terser-webpack-plugin');
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.optimization.minimize = isProd;
-    config.optimization.minimizer = [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: isProd,
-          },
-        },
-        extractComments: 'all',
-      }),
-    ];
-    return config;
+  Images: {
+    domains: [‘sekaizutto.com’],
   },
 };
 
