2b3a9897649eb1f32ae4f4a23c500d022c4bba88
yuu8812 <foir075@gmail.com>
2023-01-18 02:52:31 +0900

    hello

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -1,11 +1,18 @@
 import Main from '../component/mobile/Main';
 import { Suspense } from 'react';
+import { Box, Center } from '@chakra-ui/react';
 
 interface Props {}
 
 const mobile = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense
+      fallback={
+        <Center bg={'white'} w={'100vw'} h={'100vh'}>
+          loading
+        </Center>
+      }
+    >
       <Main />
     </Suspense>
   );
--- pages/pc.tsx
+++ pages/pc.tsx
@@ -1,9 +1,16 @@
 import Main from '../component/pc/Main';
 import { Suspense } from 'react';
+import { Box, Center } from '@chakra-ui/react';
 
 const pc = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense
+      fallback={
+        <Center bg={'white'} w={'100vw'} h={'100vh'}>
+          loading
+        </Center>
+      }
+    >
       <Main />
     </Suspense>
   );
