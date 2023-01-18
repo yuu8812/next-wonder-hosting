c1e170f8c854007a706ffc63d316b88127661abe
yuu8812 <foir075@gmail.com>
2023-01-18 13:27:31 +0900

    yeah

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const TerserPlugin = require('terser-webpack-plugin');
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.optimization.minimize = isProd;
-    config.optimization.minimizer = [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: isProd,
-          },
-        },
-        extractComments: 'all',
-      }),
-    ];
-    return config;
-  },
 };
 
 module.exports = nextConfig;
