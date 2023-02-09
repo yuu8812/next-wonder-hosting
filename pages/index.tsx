dbd6ae5d723e4132a53c31f098c4c08309105ff1
yuu8812 <foir075@gmail.com>
2023-02-09 14:25:24 +0900

    few changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         alt="mini-logo"
         width={100}
         height={100}
+        priority
       />
       <Text mt={10} fontSize="2xl" ml={5}>
         Loading...
